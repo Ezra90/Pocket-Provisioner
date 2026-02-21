882d0a31607796af1098c357c37033611ca10bc9
Ezra90 <38937043+Ezra90@users.noreply.github.com>
2026-02-21 22:58:54 +1000

    Fix missing import for FilteringTextInputFormatter in settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:file_picker/file_picker.dart';
 import '../services/button_layout_service.dart';
